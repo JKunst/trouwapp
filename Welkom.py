e0b1411b2ba08d1914e7a54366c1394bf5afbdd0
JKunst <jasperkunst00@gmail.com>
2024-06-23 21:10:33 +0200

    nieuw welkom

--- Welkom.py
+++ Welkom.py
@@ -19,7 +19,24 @@ st.markdown(
 import gspread as gs
 import pandas as pd
 from oauth2client.service_account import ServiceAccountCredentials
-gc = gs.service_account(filename='../cred.json')
+credentials = {
+    "type": st.secrets["type"],
+    "project_id": st.secrets["project_id"],
+    "private_key_id": st.secrets["private_key_id"],
+    "private_key": st.secrets["private_key"],
+    "client_email": st.secrets["client_email"],
+    "client_id": st.secrets["client_id"],
+    "auth_uri": st.secrets["auth_uri"],
+    "token_uri": st.secrets["token_uri"],
+    "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+    "universe_domain": st.secrets["universe_domain"],
+}
+gc = gs.service_account_from_dict(credentials)
+#lokaal
+#gc = gs.service_account(filename='cred.json')
+
+
 sh = gc.open('Feedback gasten')
 worksheet = sh.sheet1
 
