5b3c8d05e055acfd28150e30a7134d81d3016942
JKunst <jasperkunst00@gmail.com>
2024-07-01 21:16:49 +0200

    u of jij

--- pages/Routebeschrijving.py
+++ pages/Routebeschrijving.py
@@ -42,8 +42,8 @@ if authentication_status:
                 "De fietsenstalling is tot half 2 's nachts geopend. Check voor u vertrekt uw reis in de ns app.")
         st.markdown('''---''')
         st.subheader("Auto")
-        st.write("Met de auto kunt u parkeren op het terrein van het kasteel. \n"
-                "Rijdt door het hek rechtsom tot u op de parkeerplaats komt.")
+        st.write("Met de auto kun je parkeren op het terrein van het kasteel. \n"
+                "Rijd door het hek rechtsom tot je op de parkeerplaats komt.")
         st.markdown('''---''')
         st.subheader("Adres")
         st.write("[Hilversumsestraatweg 14, 3744 KC Baarn](https://www.google.com/maps/place//data=!4m2!3m1!1s0x47c66a89b7fcca8b:0x9c6894d987984a2f?sa=X&ved=1t:8290&ictx=111)")
